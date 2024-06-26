d772272d51dffc4cd88adac7ac7b4a0aed41c235
Kavindu Dinujaya <165418179+Dinujaya69@users.noreply.github.com>
2024-06-26 20:41:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://fitness-pack-frontend.vercel.app",
     credentials: true,
   })
 );
